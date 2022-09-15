89bd376dcf9ebc4654b6e77c4c79db406e913407
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2022-09-15 19:10:45 +0200

    made some test items and added a cool animation on the loot crate

--- src/main/java/mhd/mhd/Loot/LootCrate.java
+++ src/main/java/mhd/mhd/Loot/LootCrate.java
@@ -1,16 +1,27 @@
 package mhd.mhd.Loot;
 
+import de.tr7zw.nbtapi.NBTItem;
 import mhd.mhd.Loot.LootTables.DesertLootTable;
+import mhd.mhd.MHD;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.entity.ItemMergeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 import static mhd.mhd.Utils.ChatUtils.format;
 
@@ -28,14 +39,66 @@ public class LootCrate implements Listener {
         if (e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
             if (e.getClickedBlock().getType().equals(Material.BARREL)) {
                 Player player = e.getPlayer();
-                Location crateLocation = e.getClickedBlock().getLocation();
+                Block clickedBlock = e.getClickedBlock();
+                Location crateLocation = clickedBlock.getLocation().add(0.5, 0.5, 0.5);
+                clickedBlock.setType(Material.AIR);
 
                 player.sendMessage(format("&7Opened A Crate"));
+                player.playSound(crateLocation, Sound.ENTITY_WITHER_BREAK_BLOCK, 50.0f, 1.0f);
 
-                player.getWorld().dropItem(crateLocation, desertLoot.get(DesertLootTable.lootItems.COIN));
+                ArrayList<ItemStack> lootItems = DesertLootTable.getCrateLoot();
+
+                dropItems(lootItems, crateLocation, player);
 
                 e.setCancelled(true);
             }
+        }
+    }
+
+    void dropItems(ArrayList<ItemStack> items, Location loc, Player player) {
+        Random random = new Random();
+
+        if (items.size() > 0) {
+            int count = 0;
+            for (ItemStack item : items) {
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        Vector velocity = new Vector(random.nextDouble() * 0.15 - 0.075,0.35,random.nextDouble() * 0.15 - 0.075);
+
+                        Item dropItem = player.getWorld().dropItem(loc, item);
+                        dropItem.setVelocity(velocity);
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                dropItem.remove();
+                            }
+                        }.runTaskLater(MHD.plugin, 600);
+                    }
+                }.runTaskLater(MHD.plugin, count);
+                count++;
+            }
+        }
+    }
+
+    @EventHandler
+    private void onItemMerge(ItemMergeEvent e) {
+        NBTItem nbti = new NBTItem(e.getEntity().getItemStack());
+
+        if (!nbti.getBoolean("CanMerge")) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    private void onItemPickup(EntityPickupItemEvent e) {
+        if (e.getEntity() instanceof Player) {
+            NBTItem nbti = new NBTItem(e.getItem().getItemStack());
+            if (nbti.hasKey("CanMerge")) nbti.setBoolean("CanMerge", true);
+            nbti.applyNBT(e.getItem().getItemStack());
+        } else {
+            e.setCancelled(true);
+            e.getItem().remove();
         }
     }
 }
--- src/main/java/mhd/mhd/Loot/LootTables/DesertLootTable.java
+++ src/main/java/mhd/mhd/Loot/LootTables/DesertLootTable.java
@@ -1,10 +1,14 @@
 package mhd.mhd.Loot.LootTables;
 
+import de.tr7zw.nbtapi.NBTItem;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Random;
 
 import static mhd.mhd.Utils.ChatUtils.format;
 
@@ -13,18 +17,51 @@ public class DesertLootTable {
     public static HashMap<lootItems, ItemStack> desertDungeonLoot = new HashMap<>();
 
     public static void init() {
-        desertDungeonLoot.put(lootItems.COIN, generateItem("&6Coin", Material.SUNFLOWER));
+        desertDungeonLoot.put(lootItems.COIN, generateItem("&6Coin", Material.SUNFLOWER, 1.0));
+
+        desertDungeonLoot.put(lootItems.EMPTY_BOTTLE, generateItem("&6Empty Bottle", Material.GLASS_BOTTLE, 0.6));
+        desertDungeonLoot.put(lootItems.DUST, generateItem("&8Dust", Material.GUNPOWDER, 0.3));
     }
 
     public enum lootItems {
-        COIN
+        COIN,
+        EMPTY_BOTTLE,
+        DUST
     }
 
-    static ItemStack generateItem(String name, Material type) {
+    public static ArrayList<ItemStack> getCrateLoot() {
+        ArrayList<ItemStack> loot = new ArrayList<>();
+        int amount;
+        ItemStack tempItem;
+        Random random = new Random();
+
+        tempItem = desertDungeonLoot.get(lootItems.COIN).clone();
+        amount = random.nextInt(5) + 1;
+        for (int i = 0; i < amount; i++) loot.add(tempItem);
+
+        tempItem = desertDungeonLoot.get(lootItems.DUST).clone();
+        amount = random.nextInt(8) + 1;
+        for (int i = 0; i < amount; i++) loot.add(tempItem);
+
+        tempItem = desertDungeonLoot.get(lootItems.EMPTY_BOTTLE).clone();
+        amount = random.nextInt(3) + 1;
+        for (int i = 0; i < amount; i++) loot.add(tempItem);
+
+        Collections.shuffle(loot);
+        return loot;
+    }
+
+    static ItemStack generateItem(String name, Material type, double value) {
         ItemStack item =  new ItemStack(type);
         ItemMeta iMeta = item.getItemMeta();
         iMeta.setDisplayName(format(name));
         item.setItemMeta(iMeta);
+
+        NBTItem nbti = new NBTItem(item);
+        nbti.setString("Type", "LOOT");
+        nbti.setBoolean("CanMerge", false);
+        nbti.setDouble("Value", value);
+        nbti.applyNBT(item);
 
         return item;
     }
